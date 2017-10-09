19cad43b422fcfb12d9938a18e9a903559156529
Aaron McPeek <arn1313@comcast.net>
2017-10-09 16:57:49 -0700

    added child-actions.js

---
+++ lab-aaron/frontend/src/action/child-actions.js
@@ -0,0 +1,53 @@
+import superagent from 'superagent';
+
+export const childSet = children => ({
+  type: 'CHILD_SET',
+  payload: children,
+});
+
+export const childCreate = child => ({
+  type: 'CHILD_CREATE',
+  payload: child,
+});
+
+export const childUpdate = child => ({
+  type: 'CHILD_UPDATE',
+  payload: child,
+});
+
+export const childDelete = child => ({
+  type: 'CHILD_DELETE',
+  payload: child,
+});
+
+export const childError = err => ({
+  type: 'CHILD_ERROR',
+  payload: err,
+});
+
+export const childrenFetchRequest = () => dispatch => {
+  return superagent.get(`${__API_URL__}/api/child`)
+    .then(res => {
+      console.log(res.body);
+      dispatch(childSet(res.body));
+    })
+    .catch(console.error());
+};
+
+export const childCreateRequest = child => dispatch => {
+  return superagent.post(`${__API_URL__}/api/child`)
+    .send(child)
+    .then(res => {
+      console.log(res.body);
+      dispatch(childCreate(res.body));
+    })
+    .catch(console.error);
+};
+
+export const childDeleteRequest = child => dispatch => {
+  return superagent.delete(`${__API_URL__}/api/child/${child._id}`)
+    .then(res => {
+      dispatch(childDelete(child));
+    })
+    .catch(console.error);
+};
