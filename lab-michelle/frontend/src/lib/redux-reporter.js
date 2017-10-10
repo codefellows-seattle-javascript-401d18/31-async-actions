db65e08d1765c21970514e615194e1bc4b9ca66d
Michelle Scharlock <michelle.scharlock@gmail.com>
2017-10-09 18:19:32 -0700

    Front scaff

---
+++ lab-michelle/frontend/src/lib/redux-reporter.js
@@ -0,0 +1,13 @@
+let reporter = store => next => action => {
+  console.log('__ACTION__', action);
+
+  try{
+    let result = next(action);
+    console.log('__STATE__', store.getState());
+    return result;
+  } catch(e) {
+    e.action = action;
+    console.error('__ERROR__', e);
+    return e;
+  }
+};
