1a693ecb86ecafcd1562030e7e14c45b9624acb6
Aaron McPeek <arn1313@comcast.net>
2017-10-09 18:18:24 -0700

    finished dashboard

--- lab-aaron/frontend/src/component/dashboard/index.js
+++ lab-aaron/frontend/src/component/dashboard/index.js
@@ -5,7 +5,29 @@ import * as utils from '../../lib/util';
 import * as childActions from '../../action/child-actions';
 
 class Dashboard extends React.Component {
+  componentWillMount() {
+    this.props.childrenFetch();
+    console.log('', this.props);
+  }
 
+  render() {
+    return (
+      <div className="dashboard">
+        <h2>Which one of the Dogs has the Toy?</h2>
+        <ChildForm
+          onComplete={this.props.childCreate}
+          buttonText={'add a dog'}/>
+
+        {this.props.children.map(child =>
+          <div key={child._id}>
+            <p>{child.name}</p>
+            <button onClick={() =>
+              this.props.childDelete(child)}>X</button>
+          </div>
+        )}
+      </div>
+    );
+  }
 } //stay away from me until the end..
 
 let mapStateToProps = state => ({children: state.children});
